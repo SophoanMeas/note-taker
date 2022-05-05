19ec9ae1d313dbf2b42ca1540d8e3e6ebc6ece93
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-05 01:26:01 -0400

    add server.js

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const {saveNotes, }
+const {dbNotes} = require('../../db')
+
+router.get('/notes', (req, res) =>{
+
+    }
+
+
+module.exports = router;
